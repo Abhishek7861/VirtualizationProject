32a2302c26e53ffd042c02262cf93cc4781b702a
abhi <erabhisheksingh@outlook.com>
2019-05-02 20:32:28 +0000

    working netmap

--- CS695_project/netmap_receiver2.cpp
+++ CS695_project/netmap_receiver2.cpp
@@ -18,138 +18,142 @@ using namespace std;
 #include <thread>
 #include <mutex>
 
-
 #define ARP_REQUEST 1
 #define ARP_REPLY 2
 #define ARP_CACHE_LEN 200
 #define BACKEND_SERVERS 1
 
 struct netmap_if *nifp;
-struct netmap_ring *send_ring1, *receive_ring1;
-struct netmap_ring *send_ring2, *receive_ring2;
-struct nm_desc *d1;
-struct nm_desc *d2;
+// struct netmap_ring *send_ring1, *receive_ring1;
+// struct netmap_ring *send_ring2, *receive_ring2;
+vector<netmap_ring *> send_ring, receive_ring;
+// struct nm_desc *d1;
+// struct nm_desc *d2;
+vector<string> interface;
+vector<thread> threads;
+vector<nm_desc *> d;
 struct nmreq nmr;
-struct pollfd fds1;
-struct pollfd fds2;
+vector<pollfd> fds;
+// struct pollfd fds1;
+// struct pollfd fds2;
 int fd, length;
 mutex lockcount;
 
-
-
-
-
-void send_buffer1(char *buffer)
+void send_buffer(char *buffer,int i)
 {
     length = strlen(buffer);
-    string msg = "hello";
-    struct udp_header udphdr
-    {
-        1, 2, 3, 4
-    };
+    // string msg = "hello";
+    // struct udp_header udphdr
+    // {
+    //     1, 2, 3, 4
+    // };
 
-    struct ip_header iphdr
-    {
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-    };
-    struct wireBuffer wiremsg{udphdr.encode(),iphdr.encode(),msg};
-    char *dst = NETMAP_BUF(send_ring1, send_ring1->slot[send_ring1->cur].buf_idx);
-    memcpy(dst, (wiremsg.encode()).c_str(), (wiremsg.encode().length()));
-    send_ring1->cur = nm_ring_next(send_ring1, send_ring1->cur);
-    send_ring1->head = send_ring1->cur;
-    ioctl(fds1.fd, NIOCTXSYNC, NULL);
+    // struct ip_header iphdr
+    // {
+    //     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    // };
+    // struct wireBuffer wiremsg
+    // {
+    //     udphdr.encode(), iphdr.encode(), msg
+    // };
+    char *dst = NETMAP_BUF(send_ring[i], send_ring[i]->slot[send_ring[i]->cur].buf_idx);
+    memcpy(dst, "hello", 5);
+    send_ring[i]->cur = nm_ring_next(send_ring[i], send_ring[i]->cur);
+    send_ring[i]->head = send_ring[i]->cur;
+    ioctl(fds[i].fd, NIOCTXSYNC, NULL);
 }
 
-void send_buffer2(char *buffer)
-{
-    length = strlen(buffer);
-    string msg = "hello";
-    struct udp_header udphdr
-    {
-        1, 2, 3, 4
-    };
+// void send_buffer2(char *buffer)
+// {
+//     length = strlen(buffer);
+//     string msg = "hello";
+//     struct udp_header udphdr
+//     {
+//         1, 2, 3, 4
+//     };
 
-    struct ip_header iphdr
-    {
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-    };
-    struct wireBuffer wiremsg{udphdr.encode(),iphdr.encode(),msg};
-    char *dst = NETMAP_BUF(send_ring2, send_ring2->slot[send_ring2->cur].buf_idx);
-    memcpy(dst, (wiremsg.encode()).c_str(), (wiremsg.encode().length()));
-    send_ring2->cur = nm_ring_next(send_ring2, send_ring2->cur);
-    send_ring2->head = send_ring2->cur;
-    ioctl(fds2.fd, NIOCTXSYNC, NULL);
-}
+//     struct ip_header iphdr
+//     {
+//         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+//     };
+//     struct wireBuffer wiremsg
+//     {
+//         udphdr.encode(), iphdr.encode(), msg
+//     };
+//     char *dst = NETMAP_BUF(send_ring2, send_ring2->slot[send_ring2->cur].buf_idx);
+//     memcpy(dst, (wiremsg.encode()).c_str(), (wiremsg.encode().length()));
+//     send_ring2->cur = nm_ring_next(send_ring2, send_ring2->cur);
+//     send_ring2->head = send_ring2->cur;
+//     ioctl(fds2.fd, NIOCTXSYNC, NULL);
+// }
 
-
-
-void recvd1()
+void recvd(int i)
 {
     char *src;
-    int cur = receive_ring1->cur;
+    int cur = receive_ring[i]->cur;
     int n, rx;
 
     int count = 0;
     while (1)
     {
-        lockcount.lock();
-        poll(&fds1, 1, -1);
-        ioctl(fds1.fd, NIOCRXSYNC, NULL);
-        n = nm_ring_space(receive_ring1);
+        // lockcount.lock();
+        poll(&fds[i], 1, -1);
+        // ioctl(fds[i].fd, NIOCRXSYNC, NULL);
+        n = nm_ring_space(receive_ring[i]);
         for (rx = 0; rx < n; rx++)
         {
-            struct netmap_slot *slot = &receive_ring1->slot[cur];
-            src = NETMAP_BUF(receive_ring1, slot->buf_idx);
+            struct netmap_slot *slot = &receive_ring[i]->slot[cur];
+            src = NETMAP_BUF(receive_ring[i], slot->buf_idx);
             length = slot->len;
-            cur = nm_ring_next(receive_ring1, cur);
+            cur = nm_ring_next(receive_ring[i], cur);
         }
         count = count + rx;
-        send_buffer1(src);
-        receive_ring1->head = receive_ring1->cur = cur;
-        lockcount.unlock();
+        send_buffer(src,i);
+        receive_ring[i]->head = receive_ring[i]->cur = cur;
+        send_ring[i]->head = send_ring[i]->cur;
+        ioctl(fds[i].fd, NIOCTXSYNC, NULL);
+        // lockcount.unlock();
 
-        struct wireBuffer decodewire;
-        decodewire.decode(string(src), &decodewire);
-        cout << "1 ip = " << decodewire.ip << endl;
-        cout << "1 msg = " << decodewire.msg << endl;
-        cout << "1 udp = " << decodewire.udp << endl;
+        // struct wireBuffer decodewire;
+        // decodewire.decode(string(src), &decodewire);
+        // cout << i<<" ip = " << decodewire.ip << endl;
+        // cout << i<< "msg = " << decodewire.msg << endl;
+        // cout << i<< "udp = " << decodewire.udp << endl;
     }
 }
 
+// void recvd2()
+// {
+//     char *src;
+//     int cur = receive_ring2->cur;
+//     int n, rx;
 
-void recvd2()
-{
-    char *src;
-    int cur = receive_ring2->cur;
-    int n, rx;
+//     int count = 0;
+//     while (1)
+//     {
+//         lockcount.lock();
+//         poll(&fds2, 1, -1);
+//         ioctl(fds2.fd, NIOCRXSYNC, NULL);
+//         n = nm_ring_space(receive_ring2);
+//         for (rx = 0; rx < n; rx++)
+//         {
+//             struct netmap_slot *slot = &receive_ring2->slot[cur];
+//             src = NETMAP_BUF(receive_ring2, slot->buf_idx);
+//             length = slot->len;
+//             cur = nm_ring_next(receive_ring2, cur);
+//         }
+//         count = count + rx;
+//         send_buffer2(src);
+//         receive_ring2->head = receive_ring2->cur = cur;
+//         lockcount.unlock();
 
-    int count = 0;
-    while (1)
-    {
-        lockcount.lock();
-        poll(&fds2, 1, -1);
-        ioctl(fds2.fd, NIOCRXSYNC, NULL);
-        n = nm_ring_space(receive_ring2);
-        for (rx = 0; rx < n; rx++)
-        {
-            struct netmap_slot *slot = &receive_ring2->slot[cur];
-            src = NETMAP_BUF(receive_ring2, slot->buf_idx);
-            length = slot->len;
-            cur = nm_ring_next(receive_ring2, cur);
-        }
-        count = count + rx;
-        send_buffer2(src);
-        receive_ring2->head = receive_ring2->cur = cur;
-        lockcount.unlock();
-
-        struct wireBuffer decodewire;
-        decodewire.decode(string(src), &decodewire);
-        cout << "2 ip = " << decodewire.ip << endl;
-        cout << "2 msg = " << decodewire.msg << endl;
-        cout << "2 udp = " << decodewire.udp << endl;
-    }
-}
-
+//         struct wireBuffer decodewire;
+//         decodewire.decode(string(src), &decodewire);
+//         cout << "2 ip = " << decodewire.ip << endl;
+//         cout << "2 msg = " << decodewire.msg << endl;
+//         cout << "2 udp = " << decodewire.udp << endl;
+//     }
+// }
 
 int main()
 {
@@ -162,41 +166,53 @@ int main()
     struct arp_cache_entry *entry;
     memset(&base_req, 0, sizeof(base_req));
     base_req.nr_flags |= NR_ACCEPT_VNET_HDR;
-    d1 = nm_open("vale:4", &base_req, 0, 0);
-    d2 = nm_open("vale:2", &base_req, 0, 0);
-    fds1.fd = NETMAP_FD(d1);
-    fds2.fd = NETMAP_FD(d2);
-    fds1.events = POLLIN;
-    fds2.events = POLLIN;
-    receive_ring1 = NETMAP_RXRING(d1->nifp, 0);
-    receive_ring2 = NETMAP_RXRING(d2->nifp, 0);
-    std::cout << "number of slots" << receive_ring1->num_slots << std::endl;
-    std::cout << "buffer size" << receive_ring1->nr_buf_size << std::endl;
-    std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
-    std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
+    // d1 = nm_open("vale:4", &base_req, 0, 0);
+    // d2 = nm_open("vale:2", &base_req, 0, 0);
+    // fds1.fd = NETMAP_FD(d1);
+    // fds2.fd = NETMAP_FD(d2);
+    // fds1.events = POLLIN;
+    // fds2.events = POLLIN;
+    // receive_ring1 = NETMAP_RXRING(d1->nifp, 0);
+    // receive_ring2 = NETMAP_RXRING(d2->nifp, 0);
+    // std::cout << "number of slots" << receive_ring1->num_slots << std::endl;
+    // std::cout << "buffer size" << receive_ring1->nr_buf_size << std::endl;
+    // std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
+    // std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
 
-    std::cout << "number of slots" << receive_ring2->num_slots << std::endl;
-    std::cout << "buffer size" << receive_ring2->nr_buf_size << std::endl;
-    std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
-    std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
+    // std::cout << "number of slots" << receive_ring2->num_slots << std::endl;
+    // std::cout << "buffer size" << receive_ring2->nr_buf_size << std::endl;
+    // std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
+    // std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
 
-    send_ring1 = NETMAP_TXRING(d1->nifp, 0);
-    send_ring2 = NETMAP_TXRING(d2->nifp, 0);
-    thread th1(recvd1);
-    thread th2(recvd2);
-    th1.join();
-    th2.join();
-    
-    // int n;
-    // int timer;
-    // cout << "whats the count of threads" << endl;
-    // cin >> n;
+    // send_ring1 = NETMAP_TXRING(d1->nifp, 0);
+    // send_ring2 = NETMAP_TXRING(d2->nifp, 0);
+    // thread th1(recvd1);
+    // thread th2(recvd2);
+    // th1.join();
+    // th2.join();
+
+    int n;
+    int timer;
+    cout << "whats the count of threads" << endl;
+    cin >> n;
     // vector<thread> threads;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     threads.push_back(thread(recvd));
-    // }
-    // for (auto &t : threads)
-    //     t.join();
-    // nm_close(d);
+
+    for (int i = 0; i <= 100; i++)
+        interface.push_back("vale:" + to_string(i));
+
+    for (int i = 0; i < n; i++)
+    {
+         d.push_back(nm_open((interface[(2*i+2)]).c_str(), &base_req, 0, 0));
+        struct pollfd fds1;
+        fds1.fd = NETMAP_FD((d[i]));
+        fds1.events = POLLIN;
+        fds.push_back(fds1);
+        receive_ring.push_back(NETMAP_RXRING((d[i])->nifp, 0));
+        send_ring.push_back(NETMAP_TXRING((d[i])->nifp, 0));
+        threads.push_back(thread(recvd,i));
+    }
+    for (auto &t : threads)
+        t.join();
+    for (int i = 0; i < n; i++)
+        nm_close(d[i]);
 }
--- CS695_project/netmap_sender.cpp
+++ CS695_project/netmap_sender.cpp
@@ -17,6 +17,7 @@ using namespace std;
 #include <thread>
 #include "serialDserial.h"
 #include <mutex>
+#include <vector>
 
 #define ARP_REQUEST 1
 #define ARP_REPLY 2
@@ -24,100 +25,82 @@ using namespace std;
 #define BACKEND_SERVERS 1
 
 struct netmap_if *nifp;
-struct netmap_ring *send_ring1, *receive_ring1;
-struct nm_desc *d1;
+// struct netmap_ring *send_ring2, *receive_ring2;
+// struct netmap_ring *send_ring1, *receive_ring1;
+vector<netmap_ring*> send_ring, receive_ring;
+// struct nm_desc *d1;
+// struct nm_desc *d2;
+vector<nm_desc*> d;
+vector<string> interface;
+vector<thread> threads;
+
 struct nmreq nmr;
-struct pollfd fds1;
-struct netmap_ring *send_ring2, *receive_ring2;
-struct nm_desc *d2;
-struct pollfd fds2;
+// struct pollfd fds1;
+// struct pollfd fds2;
+vector<pollfd> fds;
 int fd, length;
 int flag = 1;
 mutex lockcount;
 mutex sendrecvlock;
 unsigned long long int globalcount = 0;
 
-void process_receive_buffer1()
+
+
+void process_receive_buffer(int i)
 {
 
-    // length = strlen(buffer);
-    string msg = "hello";
-    struct udp_header udphdr
-    {
-        1, 2, 3, 4
-    };
+    // // length = strlen(buffer);
+    // string msg = "hello";
+    // struct udp_header udphdr
+    // {
+    //     1, 2, 3, 4
+    // };
 
-    struct ip_header iphdr
-    {
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-    };
-    struct wireBuffer wiremsg
-    {
-        udphdr.encode(), iphdr.encode(), msg
-    };
-    char *dst = NETMAP_BUF(send_ring1, send_ring1->slot[send_ring1->cur].buf_idx);
-    memcpy(dst, (wiremsg.encode()).c_str(), (wiremsg.encode().length()));
-    send_ring1->cur = nm_ring_next(send_ring1, send_ring1->cur);
-    send_ring1->head = send_ring1->cur;
-    ioctl(fds1.fd, NIOCTXSYNC, NULL);
+    // struct ip_header iphdr
+    // {
+    //     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    // };
+    // struct wireBuffer wiremsg
+    // {
+    //     udphdr.encode(), iphdr.encode(), msg
+    // };
+    char *dst = NETMAP_BUF((send_ring[i]), (send_ring[i])->slot[(send_ring[i])->cur].buf_idx);
+    memcpy(dst, "hello", 5);
+    (send_ring[i])->cur = nm_ring_next((send_ring[i]), (send_ring[i])->cur);
+    (send_ring[i])->head = (send_ring[i])->cur;
+    ioctl(fds[i].fd, NIOCTXSYNC, NULL);
 }
 
-
-void process_receive_buffer2()
-{
-
-    // length = strlen(buffer);
-    string msg = "hello";
-    struct udp_header udphdr
-    {
-        1, 2, 3, 4
-    };
-
-    struct ip_header iphdr
-    {
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-    };
-    struct wireBuffer wiremsg
-    {
-        udphdr.encode(), iphdr.encode(), msg
-    };
-    char *dst = NETMAP_BUF(send_ring2, send_ring2->slot[send_ring2->cur].buf_idx);
-    memcpy(dst, (wiremsg.encode()).c_str(), (wiremsg.encode().length()));
-    send_ring2->cur = nm_ring_next(send_ring2, send_ring2->cur);
-    send_ring2->head = send_ring2->cur;
-    ioctl(fds2.fd, NIOCTXSYNC, NULL);
-}
-
-
-
-void sendnrecv1()
+void sendnrecv(int i)
 {
 
     unsigned long long int count = 0;
     while (flag)
     {
-        sendrecvlock.lock();
-        int cur = receive_ring1->cur;
+        // sendrecvlock.lock();
+        int cur = receive_ring[i]->cur;
         int n, rx;
         char *src;
-        process_receive_buffer1();
-        poll(&fds1, 1, -1);
-        ioctl(fds1.fd, NIOCRXSYNC, NULL);
-        n = nm_ring_space(receive_ring1);
+        process_receive_buffer(i);
+        poll(&fds[i], 1, -1);
+        // ioctl(fds[i].fd, NIOCTXSYNC, NULL);
+        n = nm_ring_space(receive_ring[i]);
         for (rx = 0; rx < n; rx++)
         {
-            struct netmap_slot *slot = &receive_ring1->slot[cur];
-            src = NETMAP_BUF(receive_ring1, slot->buf_idx);
+            struct netmap_slot *slot = &receive_ring[i]->slot[cur];
+            src = NETMAP_BUF(receive_ring[i], slot->buf_idx);
             length = slot->len;
-            cur = nm_ring_next(receive_ring1, cur);
+            cur = nm_ring_next(receive_ring[i], cur);
         }
-        sendrecvlock.unlock();
-
-        struct wireBuffer decodewire;
-        decodewire.decode(string(src), &decodewire);
-        cout << "1 ip = " << decodewire.ip << endl;
-        cout << "1 msg = " << decodewire.msg << endl;
-        cout << "1 udp = " << decodewire.udp << endl;
+        // sendrecvlock.unlock();
+        receive_ring[i]->head = receive_ring[i]->cur = cur;
+        send_ring[i]->head = send_ring[i]->cur;
+        ioctl(fds[i].fd, NIOCRXSYNC, NULL); 
+        // struct wireBuffer decodewire;
+        // decodewire.decode(string(src), &decodewire);
+        // cout <<i <<" ip = " << decodewire.ip << endl;
+        // cout <<i <<" msg = " << decodewire.msg << endl;
+        // cout << i<<" udp = " << decodewire.udp << endl;
         count++;
     }
     lockcount.lock();
@@ -125,41 +108,6 @@ void sendnrecv1()
     lockcount.unlock();
 }
 
-
-void sendnrecv2()
-{
-
-    unsigned long long int count = 0;
-    while (flag)
-    {
-        sendrecvlock.lock();
-        int cur = receive_ring2->cur;
-        int n, rx;
-        char *src;
-        process_receive_buffer2();
-        poll(&fds2, 1, -1);
-        ioctl(fds2.fd, NIOCRXSYNC, NULL);
-        n = nm_ring_space(receive_ring2);
-        for (rx = 0; rx < n; rx++)
-        {
-            struct netmap_slot *slot = &receive_ring2->slot[cur];
-            src = NETMAP_BUF(receive_ring2, slot->buf_idx);
-            length = slot->len;
-            cur = nm_ring_next(receive_ring2, cur);
-        }
-        sendrecvlock.unlock();
-
-        struct wireBuffer decodewire;
-        decodewire.decode(string(src), &decodewire);
-        cout << "2 ip = " << decodewire.ip << endl;
-        cout << "2 msg = " << decodewire.msg << endl;
-        cout << "2 udp = " << decodewire.udp << endl;
-        count++;
-    }
-    lockcount.lock();
-    globalcount = globalcount + count;
-    lockcount.unlock();
-}
 
 
 int main()
@@ -173,49 +121,52 @@ int main()
     struct arp_cache_entry *entry;
     memset(&base_req, 0, sizeof(base_req));
     base_req.nr_flags |= NR_ACCEPT_VNET_HDR;
-    d1 = nm_open("vale:1", &base_req, 0, 0);
-    d2 = nm_open("vale:3", &base_req, 0, 0);
-    fds1.fd = NETMAP_FD(d1);
-    fds1.events = POLLIN;
-    fds2.fd = NETMAP_FD(d2);
-    fds2.events = POLLIN;
-    receive_ring1 = NETMAP_RXRING(d1->nifp, 0);
-    receive_ring2 = NETMAP_RXRING(d2->nifp, 0);
-    std::cout << "number of slots" << receive_ring1->num_slots << std::endl;
-    std::cout << "buffer size" << receive_ring1->nr_buf_size << std::endl;
-    std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
-    std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
+    // d1 = nm_open("vale:1", &base_req, 0, 0);
+    // d2 = nm_open("vale:3", &base_req, 0, 0);
+    // fds1.fd = NETMAP_FD(d1);
+    // fds1.events = POLLIN;
+    // fds2.fd = NETMAP_FD(d2);
+    // fds2.events = POLLIN;
+    // receive_ring1 = NETMAP_RXRING(d1->nifp, 0);
+    // receive_ring2 = NETMAP_RXRING(d2->nifp, 0);
+    // std::cout << "number of slots" << receive_ring1->num_slots << std::endl;
+    // std::cout << "buffer size" << receive_ring1->nr_buf_size << std::endl;
+    // std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
+    // std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
 
-    std::cout << "number of slots" << receive_ring2->num_slots << std::endl;
-    std::cout << "buffer size" << receive_ring2->nr_buf_size << std::endl;
-    std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
-    std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
+    // std::cout << "number of slots" << receive_ring2->num_slots << std::endl;
+    // std::cout << "buffer size" << receive_ring2->nr_buf_size << std::endl;
+    // std::cout << "total buffer size" << base_req.nr_memsize << std::endl;
+    // std::cout << "rx slots, tx slots" << base_req.nr_rx_slots << " " << base_req.nr_tx_slots << std::endl;
 
-    send_ring1 = NETMAP_TXRING(d1->nifp, 0);
-    send_ring2 = NETMAP_TXRING(d2->nifp, 0);
+    // send_ring1 = NETMAP_TXRING(d1->nifp, 0);
+    // send_ring2 = NETMAP_TXRING(d2->nifp, 0);
 
-    thread th1(sendnrecv2);
-    thread th2(sendnrecv1);
-    th1.join();
-    th2.join();
-    // int n;
-    // int timer;
-    // cout << "whats the count of threads" << endl;
-    // cin >> n;
-    // cout << "whats the time of loadtest" << endl;
-    // cin >> timer;
-    // vector<thread> threads;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     threads.push_back(thread(sendnrecv));
-    // }
-    // sleep(timer);
-    // flag = 0;
-    // for (auto &t : threads)
-    //     t.join();
-    // cout << "Throughput = " << globalcount / timer << endl;
-    // close(sockfd);
-    // sendnrecv();
-    nm_close(d1);
-    nm_close(d2);
+    int n;
+    int timer;
+    cout << "whats the count of threads" << endl;
+    cin >> n;
+    cout << "whats the time of loadtest" << endl;
+    cin >> timer;
+    for (int i = 0; i <= 100; i++)
+        interface.push_back("vale:" + to_string(i));
+
+    for (int i = 0; i < n; i++)
+    {
+        d.push_back(nm_open((interface[(2*i)+1]).c_str(), &base_req, 0, 0));
+        struct pollfd fds1;
+        fds1.fd = NETMAP_FD((d[i]));
+        fds1.events = POLLIN;
+        fds.push_back(fds1);
+        receive_ring.push_back(NETMAP_RXRING((d[i])->nifp, 0));
+        send_ring.push_back(NETMAP_TXRING((d[i])->nifp, 0));
+        threads.push_back(thread(sendnrecv,i));
+    }
+    sleep(timer);
+    flag = 0;
+    for (auto &t : threads)
+        t.join();
+    cout << "Throughput = " << globalcount / timer << endl;
+    for (int i = 0; i < n; i++)
+        nm_close(d[i]);
 }
